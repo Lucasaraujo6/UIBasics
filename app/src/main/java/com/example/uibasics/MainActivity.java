c1997b18aa71b309460ceb933db8cef8be77798c
lucasaraujo6 <lucas_araujo_11@hotmail.com>
2022-02-12 05:17:38 -0300

    Listener implementation mode

--- app/src/main/java/com/example/uibasics/MainActivity.java
+++ app/src/main/java/com/example/uibasics/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,10 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnHello = findViewById(R.id.btnHello);
+
+        btnHello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Hello");
+            }
+        });
     }
 
-    public void onHelloBtnCLicked(View view){
-        TextView txtWelcome = findViewById(R.id.txtWelcome);
-        txtWelcome.setText("FODA");
-    }
+
+
 }
