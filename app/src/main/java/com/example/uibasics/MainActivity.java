4650c1636354a04555381e83d3ad8885ec1d7283
lucasaraujo6 <lucas_araujo_11@hotmail.com>
2022-02-12 17:00:02 -0300

    Implementation onLongClickListener

--- app/src/main/java/com/example/uibasics/MainActivity.java
+++ app/src/main/java/com/example/uibasics/MainActivity.java
@@ -3,23 +3,30 @@ package com.example.uibasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText edtTextHello;
+    private TextView txtWelcome;
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btnHello:
-                Toast.makeText(this, "Welcome to my aplication", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Welcome to my application", Toast.LENGTH_SHORT).show();
+                txtWelcome.setText("Welcome "+edtTextHello.getText().toString());
                 break;
             default:
                 break;
         }
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +34,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         Button btnHello = findViewById(R.id.btnHello);
-
+        edtTextHello = findViewById(R.id.edtTxtHello);
+        txtWelcome = findViewById(R.id.txtWelcome);
         btnHello.setOnClickListener(this);
+
+        btnHello.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this, "Stooop!!! It hurts.", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
 
     }
 
